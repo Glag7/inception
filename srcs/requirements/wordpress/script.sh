2f736aebe121f0cf73c8c628e67921757fae0f24
glaguyon <glaguyonie@gmail.com>
2025-02-13 15:29:21 +0100

    redis works wow

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -5,4 +5,11 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=glaguyon.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
+
+./wp-cli.phar plugin install redis-cache --activate --allow-root
+./wp-cli.phar config set WP_REDIS_HOST redis --allow-root
+./wp-cli.phar config set WP_REDIS_PORT 6379 --allow-root
+./wp-cli.phar plugin activate redis-cache --allow-root
+./wp-cli.phar redis enable --allow-root
+
 exec "$@"
