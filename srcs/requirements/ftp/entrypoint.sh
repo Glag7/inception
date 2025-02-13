a6afd6f8601aa97bc49bad8cb50b7fb5c395f230
glaguyon <glaguyonie@gmail.com>
2025-02-13 19:45:35 +0100

    wow ftp

--- srcs/requirements/ftp/entrypoint.sh
+++ srcs/requirements/ftp/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 useradd -m -p $(openssl passwd -1 $FTP_PASS) $FTP_USER
+usermod -aG www-data $FTP_USER
+
+chown -R $FTP_USER:www-data /var/www/html
+chmod -R 775 /var/www/html
 
 exec "$@"
